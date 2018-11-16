cb390456c521788a6eef3d671329b5efeb5918d0
Kaitlin <cmykaitlin@gmail.com>
2018-11-16 14:55:04 -0800

    Add login page and controller

--- src/main/java/ca/uvic/seng330/assn3/controllers/LoginController.java
+++ src/main/java/ca/uvic/seng330/assn3/controllers/LoginController.java
@@ -2,12 +2,14 @@ package ca.uvic.seng330.assn3.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class LoginController {
 
+    @GetMapping(value = "")
     @RequestMapping(value = "/login", method=RequestMethod.GET)
     public String login(ModelMap map) {
         return "login";
